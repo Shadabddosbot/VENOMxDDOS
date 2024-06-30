0f5d28bde66964aea449c6fe80c941e6476c9e68
Shadabddosbot <shadabgaming87@gmail.com>
2024-06-30 17:02:11 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7440635828:AAGnoBfY5fQU8i1f0jyxpsKyB_NA5d0IU2Q'
+ADMIN_ID = '6313155370'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
